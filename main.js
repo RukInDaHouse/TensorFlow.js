b970da5e5ed8b997632b98ed1421c815f40d16f6
RukInDaHouse <r1997p@mail.ru>
2020-09-05 23:05:14 +0300

    Add canvas

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   const video = $('#webcam')[0];
+  const ctrack = new clm.tracker();
+  ctrack.init();
 
   function onStreaming(stream) {
     video.srcObject = stream;
+    ctrack.start(video);
   }
 
   navigator.mediaDevices.getUserMedia({ video: true }).then(onStreaming);
