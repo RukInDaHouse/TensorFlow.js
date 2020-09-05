3ca89bb4f66e1567b219494291d2a8f18b88987b
RukInDaHouse <r1997p@mail.ru>
2020-09-05 23:07:27 +0300

    Add functions

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ $(document).ready(function() {
   const video = $('#webcam')[0];
   const ctrack = new clm.tracker();
   ctrack.init();
+  const overlay = $('#overlay')[0];
+const overlayCC = overlay.getContext('2d');
+
+function trackingLoop() {
+  // Проверим, обнаружено ли в видеопотоке лицо, 
+  // и если это так - начнём его отслеживать.
+  requestAnimationFrame(trackingLoop);
+
+  let currentPosition = ctrack.getCurrentPosition();
+  overlayCC.clearRect(0, 0, 400, 300);
+
+  if (currentPosition) {
+    ctrack.draw(overlay);
+  }
+}
 
   function onStreaming(stream) {
     video.srcObject = stream;
     ctrack.start(video);
+
   }
 
   navigator.mediaDevices.getUserMedia({ video: true }).then(onStreaming);
